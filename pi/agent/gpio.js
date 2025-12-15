798490c8222aa6032b7d579d6feeb10762a63abc
dijkmans <37849967+dijkmans@users.noreply.github.com>
2025-12-15 12:16:02 +0100

    Update gpio.js

--- pi/agent/gpio.js
+++ pi/agent/gpio.js
@@ -10,6 +10,15 @@ export async function initGPIO(config) {
 }
 
 export function openRolluik() {
+  if (!relay) return;
+  relay.writeSync(1);
+  setTimeout(() => relay.writeSync(0), 3000);
+}
+
+// Voorlopig dezelfde puls als OPEN.
+// Als je later 2 relais hebt (open en close), passen we dit aan.
+export function closeRolluik() {
+  if (!relay) return;
   relay.writeSync(1);
   setTimeout(() => relay.writeSync(0), 3000);
 }
