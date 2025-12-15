e61829b006a8232ff82586a8002c7dd952b017a0
dijkmans <37849967+dijkmans@users.noreply.github.com>
2025-12-15 21:35:07 +0100

    Delete pi directory

--- pi/agent/config.js
+++
@@ -1,6 +0,0 @@
-import fs from "fs";
-
-export function loadConfig() {
-  const raw = fs.readFileSync("/opt/gridbox/config/device.json");
-  return JSON.parse(raw);
-}
--- pi/scripts/update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /opt/gridbox-platform
-git pull origin main
-cd pi/agent
-npm install
-sudo systemctl restart gridbox-agent
