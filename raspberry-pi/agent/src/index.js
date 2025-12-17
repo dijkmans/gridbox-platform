23434d962c7ef427e0f9cae90bb5f7cbef6f94cc
dijkmans <37849967+dijkmans@users.noreply.github.com>
2025-12-17 10:56:00 +0100

    Update index.js

--- raspberry-pi/agent/src/index.js
+++ raspberry-pi/agent/src/index.js
@@ -1,14 +1,13 @@
-// raspberry-pi/agent/src/index.js
-
 import { startAgent } from "./agent.js";
 import { createApiClient } from "./apiClient.js";
+
+console.log("INDEX.JS WORDT UITGEVOERD");
 
 const API_BASE_URL = "https://gridbox-api-960191535038.europe-west1.run.app";
 const BOX_ID = "box-test-001";
 
-console.log("[BOOT] Agent index.js gestart");
-console.log("[BOOT] API:", API_BASE_URL);
-console.log("[BOOT] BOX_ID:", BOX_ID);
+console.log("API_BASE_URL =", API_BASE_URL);
+console.log("BOX_ID =", BOX_ID);
 
 const api = createApiClient({
   apiBaseUrl: API_BASE_URL,
