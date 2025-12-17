051f646e82bdfb16774518b3886250e239ed7206
dijkmans <37849967+dijkmans@users.noreply.github.com>
2025-12-17 09:15:23 +0100

    Update index.js

--- raspberry-pi/agent/src/index.js
+++ raspberry-pi/agent/src/index.js
@@ -1,6 +1,5 @@
 import { startAgent } from "./agent.js";
 import { createApiClient } from "./apiClient.js";
-import { createSimHardware } from "./hardware/simHardware.js";
 
 const API_BASE_URL = process.env.API_BASE_URL;
 const ORG_ID = process.env.ORG_ID;
@@ -12,11 +11,8 @@ const api = createApiClient({
   boxId: BOX_ID
 });
 
-const hardware = createSimHardware();
-
 startAgent({
   api,
-  hardware,
   config: {
     boxId: BOX_ID
   }
