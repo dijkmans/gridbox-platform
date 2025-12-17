f4073c8b2588a8406c985dbde073af10fab97be3
dijkmans <37849967+dijkmans@users.noreply.github.com>
2025-12-17 09:12:20 +0100

    Update agent.js

--- raspberry-pi/agent/src/agent.js
+++ raspberry-pi/agent/src/agent.js
@@ -1,5 +1,38 @@
 // raspberry-pi/agent/src/agent.js
 
-export function startAgent() {
-  console.log("TEST AGENT STARTED – JUISTE FILE WORDT UITGEVOERD");
+const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
+
+export function startAgent({ api, hardware, config }) {
+  const {
+    boxId,
+    pollMs = 5000,
+    heartbeatMs = 10000
+  } = config;
+
+  let shutterState = "CLOSED";
+
+  function log(...args) {
+    console.log(`[AGENT ${boxId}]`, ...args);
+  }
+
+  async function sendStatus(type = "heartbeat") {
+    await api.sendStatus({
+      shutterState,
+      type,
+      source: "agent"
+    });
+  }
+
+  async function heartbeatLoop() {
+    while (true) {
+      await sendStatus("heartbeat");
+      await sleep(heartbeatMs);
+    }
+  }
+
+  (async () => {
+    log("Agent gestart");
+    await sendStatus("startup");
+    heartbeatLoop();
+  })();
 }
